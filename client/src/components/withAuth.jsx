39aa0c0488bb8c9306ef16980a5c8062ce931f17
Brown1ee <kseniushka16@gmail.com>
2019-06-27 18:13:03 +0300

    b

--- client/src/components/withAuth.jsx
+++ client/src/components/withAuth.jsx
@@ -13,7 +13,7 @@ export default function withAuth(ComponentToProtect, shouldRedirect) {
     };
 
     componentDidMount() {
-      fetch("checkToken", {
+      fetch("https://avagram.herokuapp.com/checkToken", {
         headers: {
           "x-access-token": sessionStorage.getItem("token")
         }
